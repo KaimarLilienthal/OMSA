a9efe416b373c6f4e4c6f013c817b12fef74e0b3
Your GitHub Username <kaimarlilienthal@gmail.coml>
2023-09-21 18:25:36 +0300

    WP1

--- src/main/java/Kuehne/Nagle/OMSA/business/order/OrderService.java
+++ src/main/java/Kuehne/Nagle/OMSA/business/order/OrderService.java
@@ -3,13 +3,18 @@ package Kuehne.Nagle.OMSA.business.order;
 import Kuehne.Nagle.OMSA.business.order.dto.OrderDto;
 import Kuehne.Nagle.OMSA.business.order.dto.OrderLineDto;
 import Kuehne.Nagle.OMSA.domain.CustomerRepository;
+import Kuehne.Nagle.OMSA.domain.OrderLineRepository;
 import Kuehne.Nagle.OMSA.domain.OrderRepository;
+import Kuehne.Nagle.OMSA.domain.ProductRepository;
 import Kuehne.Nagle.OMSA.domain.entities.Customer;
 import Kuehne.Nagle.OMSA.domain.entities.Order;
+import Kuehne.Nagle.OMSA.domain.entities.OrderLine;
+import Kuehne.Nagle.OMSA.domain.entities.Product;
 import jakarta.annotation.Resource;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 @Data
 @Service
@@ -19,14 +24,29 @@ public class OrderService {
     private OrderRepository orderRepository;
     @Resource
     private CustomerRepository customerRepository;
+    @Resource
+    private ProductRepository productRepository;
+    @Resource
+    private OrderLineRepository orderLineRepository;
 
     public void addNewOrder(OrderDto orderDto) {
         Integer customerId = orderDto.getCustomerId();
         Customer customer = customerRepository.findById(customerId).get();
         Order newOrder = new Order();
         newOrder.setCustomer(customer);
+        newOrder.setOrderDate(Instant.now());
         orderRepository.save(newOrder);
-//        List<OrderLineDto> orderLines = orderDto.getOrderLines();
 
+        List<OrderLineDto> orderLines = orderDto.getOrderLines();
+        for (OrderLineDto orderLineDto :orderLines) {
+            Integer productId = orderLineDto.getProductId();
+            Product product = productRepository.findById(productId).get();
+            OrderLine newOrderLine = new OrderLine();
+            newOrderLine.setProduct(product);
+            Integer productQuantity = orderLineDto.getProductQuantity();
+            newOrderLine.setProductQuantity(productQuantity);
+            newOrderLine.setOrder(newOrder);
+            orderLineRepository.save(newOrderLine);
+        }
     }
 }
--- src/main/java/Kuehne/Nagle/OMSA/business/order/dto/OrderDto.java
+++ src/main/java/Kuehne/Nagle/OMSA/business/order/dto/OrderDto.java
@@ -1,10 +1,12 @@
 package Kuehne.Nagle.OMSA.business.order.dto;
 
+import Kuehne.Nagle.OMSA.domain.entities.Order;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.Value;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.List;
 
 /**
@@ -15,5 +17,7 @@ import java.util.List;
 public class OrderDto implements Serializable {
     @NotNull
     Integer customerId;
+    @NotNull
+    Instant orderDate;
     List<OrderLineDto> orderLines;
 }
